43996d5a3bb697c29b5ba83884d375e32fc6b0ea
gauravstr07 <gauravstr05@gmail.com>
2023-04-03 12:34:45 +0530

    first commit

---
+++ src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,19 @@
+package com.luv2code.springcoredemo.common;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+	
+	public TrackCoach() {
+		System.out.println("In constructor: " + getClass().getSimpleName());
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5k...";
+	}
+
+}
